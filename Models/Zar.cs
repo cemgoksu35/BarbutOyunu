d07809378dc314596ddea09c99099a1d76ee8ec0
Cem GÖKSU <127553434+cemgoksu35@users.noreply.github.com>
2023-05-23 11:26:18 +0300

    Add files via upload

---
+++ Models/Zar.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace Barbut.Models
+{
+    class Zar
+    {
+        //  public byte Sayi { get; set; } // Özellik olarak tanımlanırsa Zar zar=new Zar(); buna atama yapabiliriz. (Sayi=5;)
+
+        int _sayi;
+        Random _rastgele = new Random();// class-datetime-array gibi kullanımlarda newlemek zorunludur.
+
+        string _yuz;
+        public string Yuz
+        {
+
+            get
+            {
+                _yuz = "";
+                switch (_sayi)
+                {
+                    case 1:
+                        _yuz += "---\n";
+                        _yuz += "-*-\n";
+                        _yuz += "---\n";
+                        break;
+
+                    case 2:
+                        _yuz += "*--\n";
+                        _yuz += "---\n";
+                        _yuz += "--*\n";
+                        break;
+
+                    case 3:
+                        _yuz += "*--\n";
+                        _yuz += "-*-\n";
+                        _yuz += "--*\n";
+                        break;
+                    case 4:
+                        _yuz += "*-*\n";
+                        _yuz += "---\n";
+                        _yuz += "*-*\n";
+                        break;
+                    case 5:
+                        _yuz += "*-*\n";
+                        _yuz += "-*-\n";
+                        _yuz += "*-*\n";
+                        break;
+                    default:
+                        _yuz += "***\n";
+                        _yuz += "---\n";
+                        _yuz += "***\n";
+                        break;
+                }
+                return _yuz;
+            }
+
+        }
+
+
+        public int At()
+        {
+           
+            _sayi = _rastgele.Next(1, 7);// Overloadlardan minimum ve maksimum değeri olan kullanımı görüp bu kullanımı seçiyoruz.7 seçmemizin nedeni <7 mantığının olması.
+            return _sayi;
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Barbut.Models;
+
+namespace Barbut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zar zar = new Zar();
+            int sayi1 = zar.At();
+            Console.WriteLine($"Zar 1 \n{zar.Yuz}");
+            int sayi2 = zar.At();
+            Console.WriteLine($"Zar 2 \n{zar.Yuz}");
+            if (sayi1==sayi2)
+            {
+                Console.WriteLine("Berabere");
+            }
+            else if (sayi1>sayi2)
+            {
+                Console.WriteLine(sayi1 + " 1. zar kazandı " + sayi2);
+                
+            }
+            else
+            {
+                Console.WriteLine(sayi2 + " 2. zar kazandı " + sayi1);
+               
+            }
+        }
+    }
+}
